cc994bb313b82fe196398474c124bd97bf006f2d
sksusha8853 <sksusha8853@gmail.com>
2022-04-06 04:14:20 +0530

    login page added

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.get("/complaints",function(req,res){
     res.render("complaints");
 })
 
+app.get("/suggestion",function(req,res){
+    res.render("suggestion");
+})
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
